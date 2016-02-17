64de0540fccb287cb800e3a8f44db26550d178a5
Mario Sanchez Prada <mario@endlessm.com>
2016-02-17 12:02:57 +0000

    Add a way to check which decoders are available using the GstRegistry
    This a downstream patch based on the feedback from WK bug 135972, which
    enables us to specify which decoders we want to look for using the
    GstRegistry when trying to play certain mime types.
    
    For now, we only use it to match video/mp4 with a H264 decoder, but
    in the future we might use this mechanism for other types/codecs.
    
    https://bugs.webkit.org/show_bug.cgi?id=135972
    
    https://phabricator.endlessm.com/T10354

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -1690,6 +1690,25 @@ void MediaPlayerPrivateGStreamer::loadingFailed(MediaPlayer::NetworkState error)
     m_readyTimerHandler.cancel();
 }
 
+static bool gstRegistryHasDecoder(const String& decoder)
+{
+    GList *decoder_factories, *gst_decoders;
+    GstCaps *caps;
+    bool retval;
+
+    decoder_factories = gst_element_factory_list_get_elements (GST_ELEMENT_FACTORY_TYPE_DECODER | GST_ELEMENT_FACTORY_TYPE_MEDIA_VIDEO, GST_RANK_MARGINAL);
+    caps = gst_caps_new_empty_simple (decoder.utf8().data());
+    gst_decoders = gst_element_factory_list_filter (decoder_factories, caps, GST_PAD_SINK, FALSE);
+    gst_caps_unref (caps);
+
+    retval = gst_decoders != NULL;
+
+    gst_plugin_feature_list_free (gst_decoders);
+    gst_plugin_feature_list_free (decoder_factories);
+
+    return retval;
+}
+
 static HashSet<String> mimeTypeCache()
 {
     initializeGStreamerAndRegisterWebKitElements();
@@ -1768,7 +1787,6 @@ static HashSet<String> mimeTypeCache()
         "video/flv",
         "video/mj2",
         "video/mp2t",
-        "video/mp4",
         "video/mpeg",
         "video/mpegts",
         "video/ogg",
@@ -1794,6 +1812,15 @@ static HashSet<String> mimeTypeCache()
 
     for (unsigned i = 0; i < (sizeof(mimeTypes) / sizeof(*mimeTypes)); ++i)
         cache.add(String(mimeTypes[i]));
+
+    // As we ship different versions of the product with different sets of codecs,
+    // we need to make sure we don't return a MayBeSupported or IsSupported unless
+    // we are completely sure that the needed decoders are registered with the
+    // GStreamer Registry, so we do extra checks before adding the relevant types
+    // to the cache. For now we only worry about H.264, which should fallback to
+    // free codecs if a decoder is not available, but we might add more in the future.
+    if (gstRegistryHasDecoder(String::fromUTF8("video/x-h264")))
+        cache.add(String::fromUTF8("video/mp4"));
 
     typeListInitialized = true;
     return cache;
